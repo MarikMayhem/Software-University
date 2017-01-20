b8f29873ca1ce4a851cce77e20fd39b513300466
MarikMayhem <winsomewe.kek37@gmail.com>
2017-01-20 21:12:11 +0200

    Add files via upload

---
+++ Programming Fundamentals/Data Types And Variables/04. Elevator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int personNumber = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int output = (int)Math.Ceiling((double) personNumber / capacity);
+
+            Console.WriteLine(output);
+        }
+    }
+}
