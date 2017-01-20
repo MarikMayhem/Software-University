884ab05855c17188972c237a114ecf947e7e6c04
MarikMayhem <winsomewe.kek37@gmail.com>
2017-01-20 21:13:45 +0200

    Add files via upload

---
+++ Programming Fundamentals/Data Types And Variables/08. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double Height = double.Parse(Console.ReadLine());
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+            Console.Write("Height: ");
+
+            var Volume = (lenght * width * Height) / 3;
+
+            Console.WriteLine("Pyramid Volume: {0:f2}", Volume);
+            
+            
+
+        }
+    }
+}
---
+++ Programming Fundamentals/Data Types And Variables/09. Refactor Special Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int numberSum = 0; int unusualNumber = 0; 
+            for (int number = 1; number <= num; number++)
+            {
+                unusualNumber = number;
+                while (number > 0)
+                {
+                    numberSum += number % 10;
+                    number = number / 10;
+                }
+                bool condition = (numberSum == 5) || (numberSum == 7) || (numberSum == 11);
+                Console.WriteLine($"{unusualNumber} -> {condition}");
+                numberSum = 0;
+                number = unusualNumber;
+            }
+
+
+        }
+    }
+}
+
+
