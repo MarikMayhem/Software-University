e2a0a043e90e183f7f46c5d4541c24b2fa9355f7
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-03-06 21:12:27 +0200

    Wrong file removed

--- Javascript - Front End/05. Javascript Fundamentals - Exercises/14. Parse JSON Objects.js
+++
@@ -1,7 +0,0 @@
-let string = '{"name":"Gosho","age":10,"date":"19/06/2005"}';
-let object = JSON.parse(string);
-console.log(object);
-console.log(object.name);
-console.log(object.age);
-console.log(object.date);
-
--- Javascript - Front End/05. Javascript Fundamentals - Exercises/15. Turn Object into JSON string.js
+++
@@ -1,9 +0,0 @@
-let object = [
-    'name -> Angel',
-    'surname -> Georgiev',
-    'age -> 20',
-    "grade -> 6.00",
-    "date -> 23/05/1995",
-    "town -> Sofia"
-];
-console.log(JSON.stringify(object));
