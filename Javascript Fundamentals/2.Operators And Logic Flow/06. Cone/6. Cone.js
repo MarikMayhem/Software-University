b6d1941ef33e065923fff99fae7b77f3d087b14d
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-04-05 22:40:42 +0300

    2.Operators And Logic Flow Uploaded

---
+++ Javascript Fundamentals/2.Operators And Logic Flow/06. Cone/6. Cone.js
@@ -0,0 +1,7 @@
+function coneTask(a, b) {
+    let s = Math.sqrt(a * a + b * b);
+    let volume = Math.PI * a * a * b / 3;
+    console.log("volume = " + volume);
+    let area = Math.PI * a * (a + s);
+    console.log("area = " + area);
+}
