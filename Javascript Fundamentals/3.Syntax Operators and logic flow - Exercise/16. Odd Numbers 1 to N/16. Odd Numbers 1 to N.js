d8f9449b617b41b2f8a887ae992e6c6998c61002
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2018-04-09 11:42:38 +0300

    Third part uploaded

---
+++ Javascript Fundamentals/3.Syntax Operators and logic flow - Exercise/16. Odd Numbers 1 to N/16. Odd Numbers 1 to N.js
@@ -0,0 +1,9 @@
+function findOddNums(number){
+
+    for (let i = 0; i <= number; i++) {
+
+        if (i % 2 !== 0){
+            console.log(i);
+        }
+    }
+}
---
+++ Javascript Fundamentals/3.Syntax Operators and logic flow - Exercise/17. Triangle of Dollars/17. Triangle of Dollars.js
@@ -0,0 +1,10 @@
+function triangleOfDollars(input){
+
+    let dollar = "$";
+    for (let i = 0; i < input; i++) {
+
+        console.log(dollar);
+        dollar += "$";
+
+    }
+}
